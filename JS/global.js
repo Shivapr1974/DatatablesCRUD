ef3b70c0bfbace47935f2e39cec9cfaf86c053f0
Siva Ram <Sivakumar.1.Ramakrishnan@gm.com>
2020-01-03 10:01:54 -0500

    Init

---
+++ JS/global.js
@@ -0,0 +1,45 @@
+        function setCookie(key, value) {
+            var expires = new Date();
+            expires.setTime(expires.getTime() + (10*365 * 24 * 60 * 60 * 1000));
+            document.cookie = key + '=' + value + ';expires=' + expires.toUTCString();
+        }
+
+        function getCookie(key) {
+            var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
+            return keyValue ? keyValue[2] : null;
+        }
+  
+        function findAndReplace(string, target, replacement) {
+        	 var i = 0, length = string.length;
+        	 for (i; i < length; i++) {
+        	   string = string.replace(target, replacement);
+        	 }
+        	 return string;
+        	 
+       	}        
+        
+        function openWorkList( id) {
+            window.open("/FraudGUI/detail.jsp?TABLE=worklist&ID="+ id, "", "width=1200,height=600");
+        }   
+        
+        
+        function updateStatus(select, order_id, id) {
+        	var url="/FraudGUI/DBDataServlet?type=Update&Order_Id="+
+        				order_id+"&Status="+select.value;
+        	console.log(url);
+        	$.ajax(url, {
+        	      success: function(data) {
+        	    	  if(data.indexOf("Success")>=0){
+        	    			console.log("Update Successful");
+        	    		    location.reload();
+        	    	  }else{
+        	 	         alert("Updated failed");
+        	    	  }
+        	      },
+        	      error: function() {
+        	         alert("Failed update");
+        	      }
+        	   });
+        }
+
+        
