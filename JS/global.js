6b2e9654fe025af8b81a8642c2a0febfdc4557b3
Siva Ram <Sivakumar.1.Ramakrishnan@gm.com>
2020-01-06 14:46:36 -0500

    Report Changes

--- JS/global.js
+++ JS/global.js
@@ -12,6 +12,10 @@
 		function checkNull(str){
 			return (str === "" || str === null || str === undefined);
 		}
+		Date.prototype.getWeek = function() {
+			var onejan = new Date(this.getFullYear(), 0, 1);
+			return Math.ceil((((this - onejan) / 86400000) + onejan.getDay() + 1) / 7);
+		}		
 		
 		function setCookie(key, value) {
             var expires = new Date();
