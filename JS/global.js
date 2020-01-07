73c9757f4b9ce03090dc01e29da2fde19f9275ad
Siva Ram <Sivakumar.1.Ramakrishnan@gm.com>
2020-01-07 11:26:52 -0500

    IE related changes

--- JS/global.js
+++ JS/global.js
@@ -1,3 +1,4 @@
+		var month = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 		function setIcons(i){
 			detail = "<img class=imgdetail  src='IMAGES/details.png' onclick=\"handleClick('details', " + i +")\">";
 			edit =   "<img class=imgdetail  src='IMAGES/edit.png' onclick=\"handleClick('edit', " + i +")\">";
@@ -10,14 +11,39 @@
 			}
 		}
 		function checkNull(str){
-			return (str === "" || str === null || str === undefined);
+			return (str === "" || str === null || str === undefined || str === "undefined");
 		}
 		Date.prototype.getWeek = function() {
 			var onejan = new Date(this.getFullYear(), 0, 1);
 			return Math.ceil((((this - onejan) / 86400000) + onejan.getDay() + 1) / 7);
 		}		
+		Date.prototype.getMonthStr = function() {
+			return month[this.getMonth()];
+		}		
+
+		function getQueryString() {
+			var key = false, res = {}, itm = null;
+			// get the query string without the ?
+			var qs = location.search.substring(1);
+			// check for the key as an argument
+			if (arguments.length > 0 && arguments[0].length > 1)
+			  key = arguments[0];
+			// make a regex pattern to grab key/value
+			var pattern = /([^&=]+)=([^&]*)/g;
+			// loop the items in the query string, either
+			// find a match to the argument, or build an object
+			// with key/value pairs
+			while (itm = pattern.exec(qs)) {
+			  if (key !== false && decodeURIComponent(itm[1]) === key)
+				return decodeURIComponent(itm[2]);
+			  else if (key === false)
+				res[decodeURIComponent(itm[1])] = decodeURIComponent(itm[2]);
+			}
+  
+			return key === false ? res : null;
+		  }		
 		
-		function setCookie(key, value) {
+		 function setCookie(key, value) {
             var expires = new Date();
             expires.setTime(expires.getTime() + (10*365 * 24 * 60 * 60 * 1000));
             document.cookie = key + '=' + value + ';expires=' + expires.toUTCString();
@@ -27,7 +53,27 @@
             var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
             return keyValue ? keyValue[2] : null;
         }
-  
+		function getCookieAndParse(key, nullVal) {
+			var str = getCookie(key);
+			if(checkNull(str)) return nullVal;
+            return JSON.parse(str); ;
+		}
+		function setData(key, data){
+			try{ localStorage.setItem(key, JSON.stringify(data)); 
+			}catch(e){ 
+				setCookie(key, JSON.stringify(data)); 
+			}			
+		}
+		function getData(key, nullVal){
+			var origDataSet;
+			try{ origDataSet = JSON.parse(localStorage.getItem(key)); 
+			}catch(e){ 
+				origDataSet = getCookieAndParse(key, nullVal); 
+			}			
+			return origDataSet;
+		}	
+
+
         function findAndReplace(string, target, replacement) {
         	 var i = 0, length = string.length;
         	 for (i; i < length; i++) {
